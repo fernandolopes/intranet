b363c672c7bda8e3b9d9da655878be97d57d52cd
Fernando Lopes <fernandolopes.s@gmail.com>
2011-05-31 12:56:36 -0300

    Modificado umas regras de permissão e para abri uma nova janela ao acessar um relatório de ponto.

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -2,9 +2,10 @@
 class ReportController < ActionController::Base
   protect_from_forgery
 
+
   rescue_from CanCan::AccessDenied do |exception|
 		flash[:error] = "Access denied!"
 		redirect_to root_url
-	end
+  end
 
 end
